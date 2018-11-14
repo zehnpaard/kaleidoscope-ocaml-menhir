dcc31941f91263169b7f61133545357abc174941
zehnpaard <zehnpaard@googlemail.com>
2018-11-14 22:40:20 +0900

    Update type expressions

--- ast.ml
+++ ast.ml
@@ -1,10 +1,10 @@
-type expr =
-  | Number of float
-  | Variable of string
-  | Binary of char * expr * expr
-  | Call of string * expr list
-;;
+type expr = [
+  | `Number of float
+  | `Variable of string
+  | `Binary of char * expr * expr
+  | `Call of string * expr list
+]
 
-type proto = Prototype of string * string array;;
-type func = Function of proto * expr;;
-type toplevel = TExtern of proto | TFunction of func;;
+type proto = [ `Prototype of string * string list ]
+type func = [ `Function of proto * expr ]
+type toplevel = [ `TExtern of proto | `TFunction of func ]

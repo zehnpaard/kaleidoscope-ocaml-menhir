9730645810335e855a6b21eff5c34c50360bba88
zehnpaard <zehnpaard@googlemail.com>
2018-11-15 07:47:35 +0900

    Add LLVM code generation for arithmetic operations

--- 02_arithmetic/codegen.ml
+++ 02_arithmetic/codegen.ml
@@ -7,5 +7,14 @@ let the_module = create_module context "minimal"
 let builder = builder context
 let double_type = double_type context
 
-let codegen_expr = function
+exception InvalidOperator of char
+let rec codegen_expr = function
   | `Number n -> const_float double_type n
+  | `BinOp (op, e1, e2) ->
+        let lhs = codegen_expr e1 in
+        let rhs = codegen_expr e2 in
+        (match op with
+           | '+' -> build_add lhs rhs "addtmp" builder
+           | '-' -> build_sub lhs rhs "subtmp" builder
+           | '*' -> build_mul lhs rhs "multmp" builder
+           | _ -> raise (InvalidOperator op))

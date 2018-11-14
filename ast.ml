0500d8ab25a4bbb5fe92e4448effc5cfbe9823fb
zehnpaard <zehnpaard@googlemail.com>
2018-11-14 20:41:53 +0900

    Modify AST and add toplevel type

--- ast.ml
+++ ast.ml
@@ -2,8 +2,9 @@ type expr =
   | Number of float
   | Variable of string
   | Binary of char * expr * expr
-  | Call of string * expr array
+  | Call of string * expr list
 ;;
 
 type proto = Prototype of string * string array;;
 type func = Function of proto * expr;;
+type toplevel = TExtern of proto | TFunction of func;;

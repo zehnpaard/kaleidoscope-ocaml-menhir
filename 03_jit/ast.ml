8fdfab88071c03b9722b6e11e33fb0e82ad821a2
zehnpaard <zehnpaard@googlemail.com>
2018-11-16 02:06:42 +0900

    Add toplevel main, prototype and function types to AST

--- 03_jit/ast.ml
+++ 03_jit/ast.ml
@@ -2,3 +2,8 @@ type expr = [
   | `Number of float
   | `BinOp of char * expr * expr
 ]
+
+type proto = [ `Prototype of string * string list ]
+type func = [ `Function of proto * expr ]
+
+type toplevel = [ `TLMain of func ]
